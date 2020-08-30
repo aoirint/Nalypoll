e6b90e35bd44a92aad761954d5a60ce418062470
aoirint <aoirint@gmail.com>
2020-08-31 03:08:39 +0900

    delta check update environ

--- Nalypoll/tools/PollUpdater.py
+++ Nalypoll/tools/PollUpdater.py
@@ -79,8 +79,8 @@ if __name__ == '__main__':
     import schedule
 
     updater = PollUpdater(
-        delta_check_tweets=timedelta(minutes=1),
-        delta_check_users=timedelta(minutes=1),
+        delta_check_tweets=timedelta(minutes=int(os.environ.get('DELTA_CHECK_TWEETS', 15))),
+        delta_check_users=timedelta(minutes=int(os.environ.get('DELTA_CHECK_USERS', 60))),
     )
     connection.close() # explicitly close connection
 
